ca69edc010cd5967cdb195c668fa04f33d78d1ee
hardycathal@gmail.com <hardyfcathal@gmail.com>
2024-10-23 14:16:12 +0100

    Created new project

---
+++ src/main/java/ie/atu/week6y3/Week6Y3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week6y3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6Y3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week6Y3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week6y3/Week6Y3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week6y3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week6Y3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
