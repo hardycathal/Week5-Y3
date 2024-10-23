77c0ad0bab592a7a184888c6b8fd497242371a1e
hardycathal@gmail.com <hardyfcathal@gmail.com>
2024-10-23 14:34:14 +0100

    Starting lab

---
+++ src/main/java/ie/atu/week6y3/ProductController.java
@@ -0,0 +1,27 @@
+package ie.atu.week6y3;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private ProductService myProduct;
+
+    public ProductController(ProductService myProduct){
+        this.myProduct = myProduct;
+    }
+
+
+    @PostMapping("/newProduct")
+    public List<Product> newProduct(@RequestBody Product product){
+
+        // business logic to ass this to a database
+        // return list of ll products from database
+        return myProduct.addProduct(product);
+    }
+}
